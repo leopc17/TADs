9e020fb565de32597a9ded40fe304bc73a4f1353
leopc17 <LeonardoEAF45@gmail.com>
2025-07-15 14:11:51 -0300

    feat(stack)!: adiciona lançamento de exceções nos métodos da Stack
    - Método push() lança exceção caso a pilha esteja cheia
    - Métodos top() e pop() lançam exceção caso a pilha esteja vazia
    
    BREAKING CHANGE: Métodos push(), top() e pop() podem lançar exceções

--- src/main/java/org/example/dataStructures/interfaces/Stack.java
+++ src/main/java/org/example/dataStructures/interfaces/Stack.java
@@ -1,10 +1,13 @@
 package org.example.dataStructures.interfaces;
 
+import org.example.dataStructures.exceptions.stack.EmptyStackException;
+import org.example.dataStructures.exceptions.stack.FullStackException;
+
 public interface Stack<T> extends Iterable<T> {
 
-    void push(T element);
-    T pop();
-    T top();
+    void push(T element) throws FullStackException;
+    T pop() throws EmptyStackException;
+    T top() throws EmptyStackException;
     Integer size();
     boolean isEmpty();
 }
